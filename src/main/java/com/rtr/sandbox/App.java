e1373eac79bdaa32f5b01abe1cfa6b1b95c30273
John Hite <jhite@renttherunway.com>
2013-01-08 09:39:53 -0500

    adding test code

---
+++ src/main/java/com/rtr/sandbox/App.java
@@ -0,0 +1,13 @@
+package com.rtr.sandbox;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
